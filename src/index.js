465ab082240390b2bb986255363a461444992344
JoshuaMasterton <joshuamasterton@protonmail.com>
2022-12-06 10:40:46 +0000

    Project push

--- src/index.js
+++ src/index.js
@@ -1,104 +1,165 @@
 import "./index.css";
+import logoImg from "./img/logo.png";
 
-const task = (() => {
-    const taskArray = [];
+// Get current date
 
-    const changeMain = (e, getMain) => {
-        e.preventDefault();
-        let target = e.target.innerHTML;
-        while(getMain.lastElementChild){
-            getMain.removeChild(getMain.lastElementChild);
-        };
-        displayController.buildMain(target);
-    };
+const currentDate = () => {
+    const date = new Date();
 
-    return {changeMain};
-})();
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const year  = String(date.getFullYear());
 
-const displayController = (() => {
+    return `${year}-${month}-${day}`;
+};
+
+// Array containing all data for tasks
+
+let taskArray = [
+    {id: 0, title: "Wash", des: "Clean the clothes", due: currentDate()},
+];
+
+const display = (() => {
     const getTop = document.getElementById("topCon");
     const getLeft = document.getElementById("leftCon");
     const getMain = document.getElementById("mainCon");
 
-    const buildLeft = () => {
-        const allTask = document.createElement("button");
-        allTask.className = "left";
-        allTask.innerHTML = "All Tasks";
-        allTask.addEventListener("click",(e) => {
-            task.changeMain(e, getMain);
-        });
-        getLeft.appendChild(allTask);
+    // All DOM elements associated with top bar go under top function
 
-        const today = document.createElement("button");
-        today.className = "left";
-        today.innerHTML = "Today";
-        today.addEventListener("click", (e) => {
-            task.changeMain(e, getMain);
-        });
-        getLeft.appendChild(today);
+    const top = () => {};
 
-        const upcoming = document.createElement("button");
-        upcoming.className = "left";
-        upcoming.innerHTML = "Upcoming";
-        upcoming.addEventListener("click", (e) => {
-            task.changeMain(e, getMain);
-        });
-        getLeft.appendChild(upcoming);
+    // All DOM elements associated with left bar go under left function
+
+    const left = () => {
+        const leftTask = document.createElement("button");
+        leftTask.className = "left";
+        leftTask.id = "leftTask";
+        leftTask.innerHTML = "Tasks";
+        leftTask.addEventListener("click", (e) => task.changeMain(e, getMain));
+        getLeft.appendChild(leftTask);
 
         const addTask = document.createElement("button");
-        addTask.classList = "left";
+        addTask.className = "left";
+        addTask.id = "addTask";
         addTask.innerHTML = "Add Task";
-        addTask.addEventListener("click", (e) => {
-            task.changeMain(e, getMain);
-        });
+        addTask.addEventListener("click", (e) => task.changeMain(e, getMain));
         getLeft.appendChild(addTask);
     };
 
-    const buildMain = (target) => {
-        const buildAllTask = () => {
-            const allTask = document.createElement("div");
-            allTask.className = "main";
-            allTask.id = "allTask";
-            allTask.innerHTML = "All Tasks";
-            getMain.appendChild(allTask);
+    // All DOM elements associated with main bar go under main function
+
+    const main = () => {
+        const mainTask = () => {
+            for(let i = 0; i < taskArray.length; i++){
+                const mainTaskCon = document.createElement("div");
+                mainTaskCon.className = "mainTaskCon";
+                getMain.appendChild(mainTaskCon);
+                
+                    const mainTaskTitle = document.createElement("div");
+                    mainTaskTitle.className = "mainTask";
+                    mainTaskTitle.innerHTML = taskArray[i].title;
+                    mainTaskCon.appendChild(mainTaskTitle);
+
+                    const mainTaskDes = document.createElement("p");
+                    mainTaskDes.className = "mainTask";
+                    mainTaskDes.innerHTML = taskArray[i].des;
+                    mainTaskCon.appendChild(mainTaskDes);
+
+                    const mainTaskDue = document.createElement("div");
+                    mainTaskDue.className = "mainTask";
+                    mainTaskDue.innerHTML = taskArray[i].due;
+                    mainTaskCon.appendChild(mainTaskDue);
+
+                    const mainTaskRemove = document.createElement("button");
+                    mainTaskRemove.className = "mainTask";
+                    mainTaskRemove.innerHTML = `Remove${taskArray[i].id}`;
+                    mainTaskRemove.value = parseInt(taskArray[i].id);
+                    mainTaskRemove.addEventListener("click",(e) => task.removeTask(e, getMain));
+                    mainTaskCon.appendChild(mainTaskRemove);
+            };
         };
 
-        const buildToday = () => {
-            const today = document.createElement("div");
-            today.className = "main";
-            today.id = "today";
-            today.innerHTML = "Today";
-            getMain.appendChild(today);  
+        // Add task through a form
+
+        const addTask = () => {
+            const addForm = document.createElement("form");
+            addForm.className = "addTaskCon";
+            getMain.appendChild(addForm);
+
+                const addTitle = document.createElement("input");
+                addTitle.className = "addTask";
+                addTitle.placeholder = "Title";
+                addForm.appendChild(addTitle);
+
+                const addDes = document.createElement("textarea");
+                addDes.className = "addTask";
+                addDes.placeholder = "Description";
+                addForm.appendChild(addDes);
+
+                const addDue = document.createElement("input");
+                addDue.className = "addTask";
+                addDue.type = "date";
+                addForm.appendChild(addDue);
+
+                const addSubmit = document.createElement("button");
+                addSubmit.className = "addTask";
+                addSubmit.innerHTML = "Add Task";
+                addSubmit.addEventListener("click", (e) => {
+                    task.addTaskSubmit(
+                        e, getMain, taskArray.length, addTitle.value, addDes.value, addDue.value
+                    );
+                });
+                addForm.appendChild(addSubmit);
         };
 
-        const buildUpcoming = () => {
-            const upcoming = document.createElement("div");
-            upcoming.className = "main";
-            upcoming.id = "upcoming";
-            upcoming.innerHTML = "Upcoming";
-            getMain.appendChild(upcoming);
-        };
-        
-        const buildAddTask = () => {
-            const addTask = document.createElement("div");
-            addTask.className = "main";
-            addTask.id = "addTask";
-            addTask.innerHTML = "Add Task";
-            getMain.appendChild(addTask);
-        };
-
-        if(target === "All Tasks"){return buildAllTask()}
-        else if(target === "Today"){return buildToday()}
-        else if(target === "Upcoming"){return buildUpcoming()}
-        else if(target === "Add Task"){return buildAddTask()}
-        else{return buildAllTask()};
+        return {mainTask, addTask};
     };
 
-    return {
-        buildLeft, 
-        buildMain,
-    };
+    return {top, left, main};
 })();
 
-displayController.buildMain();
-displayController.buildLeft();
+const task = (() => {
+
+    // Execute left and main function for inital start of webpage
+
+    display.left();
+    display.main();
+    display.main().mainTask();
+
+    // Function for addTask array add
+
+    const AddTask = (id, title, des, due) => {
+        return {id, title, des, due};
+    };    
+
+    // Use left bar to choose which main page to choose
+
+    const changeMain = (e, getMain) => {
+        e.preventDefault();
+        while(getMain.lastElementChild){getMain.removeChild(getMain.lastElementChild)};
+        if(e.target.id === "addTask"){display.main().addTask()}
+        else if(e.target.id === "leftTask"){display.main().mainTask()}
+        else{display.main().mainTask()};
+    };
+
+    const addTaskSubmit = (e, getMain, id, title, des, due) => {
+        e.preventDefault();
+        const newTask = AddTask(id, title, des, due);
+        taskArray.push(newTask);
+        while(getMain.lastElementChild){getMain.removeChild(getMain.lastElementChild)};
+        display.main().mainTask();
+    };
+
+    const removeTask = (e, getMain) => {
+        e.preventDefault();
+        const newArray = taskArray.filter((obj) => obj.id != e.target.value);
+        taskArray = newArray;
+        for(let i = 0; i < taskArray.length; i++){
+            taskArray[i].id = i;
+        };
+        while(getMain.lastElementChild){getMain.removeChild(getMain.lastElementChild)};
+        display.main().mainTask();
+    };
+
+    return {changeMain, addTaskSubmit, removeTask};
+})();
